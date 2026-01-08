011558577c766f10d13b9967fa7ac7541db75a0b
Ananya Budati <144731515+Ananya8989@users.noreply.github.com>
2026-01-08 11:24:32 -0600

    Update align_reads.sh

--- align_reads.sh
+++ align_reads.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+#Align trimmed RNA reads to reference genome using HISAT2 tool
 for file in *.fastq; do
     filename=$(basename "$file" .fastq)
     hisat2 -q --rna-strandness R -x grcm38/genome -U $file | samtools sort -o "$filename"_sorted.bam
